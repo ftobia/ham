1647db2b5005233e050fc7aac43227996947dae9
ftobia <frank.tobia@gmail.com>
2013-09-16 22:34:51 -0400

    Pull some imports out of test functions.

--- tests/test_pronunciation.py
+++ tests/test_pronunciation.py
@@ -1,26 +1,24 @@
 import pytest
 
+from ham import Pronunciation
+
 
 class TestPronunciation(object):
 
     def test_constructor(self):
-        from ham import Pronunciation
         ink = Pronunciation(['IH1', 'NG', 'K'])
         ink2 = Pronunciation(ink)
         assert ink == ink2
         assert ink is not ink2
 
     def test_str(self):
-        from ham import Pronunciation
         assert str(Pronunciation(['HH', 'AO1', 'NG', 'K'])) == 'HH AO1 NG K'
 
     def test_repr(self):
-        from ham import Pronunciation
         assert repr(Pronunciation(['M', 'AH1', 'NG', 'K', 'IY0'])) == \
             '<Pronunciation "M AH1 NG K IY0">'
 
     def test_iter(self):
-        from ham import Pronunciation
         phonemes = ['G', 'L', 'AE1', 'S']
         pronunciation = Pronunciation(phonemes)
         i = iter(pronunciation)
@@ -30,7 +28,6 @@ class TestPronunciation(object):
         assert list(pronunciation) == phonemes
 
     def test_contains(self):
-        from ham import Pronunciation
         from ham.symbols import AH, AH0, AH1, AH2, IY, IY0, IY1, IY2
         monkey = Pronunciation(['M', 'AH1', 'NG', 'K', 'IY0'])
         assert AH in monkey
@@ -43,13 +40,11 @@ class TestPronunciation(object):
         assert IY2 not in monkey
 
     def test_contains_incorrect_types(self):
-        from ham import Pronunciation
         word = Pronunciation(['W', 'ER1', 'D'])
         assert None not in word
         assert word not in word
 
     def test_eq(self):
-        from ham import Pronunciation
         hello = Pronunciation(['HH', 'AH0', 'L', 'OW1'])
         assert hello == hello
         assert hello == Pronunciation(hello)
@@ -57,7 +52,6 @@ class TestPronunciation(object):
         assert not (hello == Pronunciation(['G', 'UH2', 'D', 'B', 'AY1']))
 
     def test_ne(self):
-        from ham import Pronunciation
         assert Pronunciation(['HH', 'AH0', 'L', 'OW1']) != \
             Pronunciation(['G', 'UH2', 'D', 'B', 'AY1'])
         assert Pronunciation(['HH', 'AH0', 'L', 'OW1']) != \
@@ -66,14 +60,12 @@ class TestPronunciation(object):
                     Pronunciation(['HH', 'AH0', 'L', 'OW1']))
 
     def test_len(self):
-        from ham import Pronunciation
         assert len(Pronunciation(['AH0'])) == 1
         assert len(Pronunciation(['HH', 'AY1'])) == 2
         assert len(Pronunciation(['G', 'L', 'OW1'])) == 3
         assert len(Pronunciation(['K', 'L', 'IH1', 'P'])) == 4
 
     def test_index(self):
-        from ham import Pronunciation
         p = Pronunciation(['M', 'IH2', 'S', 'IH0', 'S', 'IH1', 'P', 'IY0'])
         assert p.index('M') == 0
         assert p.index('S') == 2
--- tests/test_word.py
+++ tests/test_word.py
@@ -1,25 +1,23 @@
 import pytest
 
+from ham import Word
+
 
 class TestWord(object):
 
     def test_constructor(self):
-        from ham import Word
         slap = Word('slap')
         slap2 = Word(slap)
         assert slap == slap2
         assert slap is not slap2
 
     def test_str(self):
-        from ham import Word
         assert str(Word('foo')) == 'foo'
 
     def test_repr(self):
-        from ham import Word
         assert repr(Word('foo')) == '<Word "foo">'
 
     def test_iter(self):
-        from ham import Word
         word = Word('hello')
         i = iter(word)
         assert ''.join(i) == 'hello'
@@ -28,7 +26,6 @@ class TestWord(object):
         assert ''.join(word) == 'hello'
 
     def test_contains(self):
-        from ham import Word
         foo = Word('foo')
         assert 'f' in foo
         assert 'o' in foo
@@ -40,7 +37,6 @@ class TestWord(object):
         assert 'foof' not in foo
 
     def test_eq(self):
-        from ham import Word
         foo = Word('foo')
         assert foo == foo
         assert foo == Word('foo')
@@ -48,13 +44,11 @@ class TestWord(object):
         assert not (foo == Word('monkey'))
 
     def test_ne(self):
-        from ham import Word
         assert Word('monkey') != Word('butler')
         assert Word('monkey') != 'monkey'
         assert not (Word('monkey') != Word('monkey'))
 
     def test_pop(self):
-        from ham import Word
         w = Word('foo')
         assert w.pop('f') == 'f'
         assert str(w) == '.oo'
@@ -64,26 +58,22 @@ class TestWord(object):
         assert str(w) == '...'
 
     def test_pop_nonexistent(self):
-        from ham import Word
         w = Word('foo')
         with pytest.raises(ValueError) as excinfo:
             w.pop('a')
         assert str(excinfo.value) == '"a" is not in word'
 
     def test_pop_single_letter_from_middle_of_word(self):
-        from ham import Word
         w = Word('primary')
         assert w.pop('m') == 'm'
         assert str(w) == 'pri.ary'
 
     def test_pop_multiple_letters(self):
-        from ham import Word
         w = Word('monkey')
         assert w.pop('onk') == 'onk'
         assert str(w) == 'm...ey'
 
     def test_len(self):
-        from ham import Word
         key = 'key'
         assert len(Word(key)) == len(key)
         monkey = 'monkey'
@@ -93,7 +83,6 @@ class TestWord(object):
         assert len(w) == len(monkey) - len(key)
 
     def test_vowel_groups(self):
-        from ham import Word
         assert list(Word('hello').vowel_groups()) == ['e', 'o']
         assert list(Word('monkey').vowel_groups()) == ['o', 'ey']
         assert list(Word('toast').vowel_groups()) == ['oa']
